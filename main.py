38f5bfffaaec8d1307ef8dc1b6425036193f98be
Loxnebot <loxnebot@gmail.com>
2025-10-31 20:16:31 +0200

    dsa

---
+++ main.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+con = sqlite3.connect("movie.db")
+cur = con.cursor()
+
+cur.execute("SELECT title, popularity FROM movies ORDER BY popularity DESC LIMIT 1")
+print(cur.fetchall())
+
+cur.execute("SELECT title, budget FROM movies WHERE release_date LIKE '2009%' ORDER BY budget DESC LIMIT 1")
+print(cur.fetchall())
+
+cur.execute("""SELECT title FROM movies WHERE tagline LIKE '%The battle within%' """)
+print(cur.fetchall())
+
+cur.execute("SELECT vote_count FROM movies WHERE release_date < '1980' and vote_average > 8 ORDER BY vote_count DESC LIMIT 1")
+print(cur.fetchall())
+
+
+
