8c9b6701c1267d761c3557edf5ffba569d7808d6
Loxnebot <loxnebot@gmail.com>
2025-11-01 17:09:09 +0200

    ds

--- main.py
+++ main.py
@@ -14,3 +14,40 @@ print(cur.fetchall())
 
 cur.execute("SELECT vote_count FROM movies WHERE release_date < '1980' and vote_average > 8 ORDER BY vote_count DESC LIMIT 1")
 print(cur.fetchall())
+
+cur.execute("""SELECT directors.name, MAX(movies.release_date) as last_movie_date 
+        FROM movies
+        INNER JOIN directors ON movies.director_id = directors.id
+        WHERE directors.name IS NOT NULL
+        GROUP BY directors.name
+        ORDER BY last_movie_date DESC
+            """)
+print(cur.fetchall())
+
+cur.execute("""SELECT directors.name, SUM(movies.budget) as total_budget
+        FROM movies
+        INNER JOIN directors ON movies.director_id = directors.id
+        WHERE directors.name IS NOT NULL
+        GROUP BY directors.name""")
+
+cur.execute("""SELECT directors.name, COUNT(movies.id) as times_directed 
+    FROM movies 
+    INNER JOIN directors ON movies.director_id = directors.id 
+    WHERE directors.name IS NOT NULL 
+    GROUP BY directors.name 
+    ORDER BY times_directed DESC""")
+
+cur.execute("""SELECT movies.title, COUNT(movies_genres.genre_id) as genre_count
+        FROM movies_genres
+        INNER JOIN movies ON movies.id = movies_genres.movie_id
+        WHERE movies_genres.movie_id IS NOT NULL
+        GROUP BY movies.title
+        ORDER BY genre_count DESC """)
+
+cur.execute("""SELECT directors.name, COUNT(movies_genres.genre_id) as genre_count
+            FROM movies_genres
+            INNER JOIN movies ON movies.id = movies_genres.movie_id
+            INNER JOIN directors ON movies.director_id = directors.id
+            WHERE directors.name IS NOT NULL
+            GROUP BY directors.name
+            ORDER BY genre_count DESC""")
